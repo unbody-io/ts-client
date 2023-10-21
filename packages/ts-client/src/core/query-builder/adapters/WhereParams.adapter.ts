44acbf9f19363d0e9aa917dc87a5f3dd84bad8f2
Milad Azhdehnia <miladazhdehnia@gmail.com>
2023-10-22 02:42:15 +0330

    feat(ts-client): add cross reference adapter

--- packages/ts-client/src/core/query-builder/adapters/WhereParams.adapter.ts
+++ packages/ts-client/src/core/query-builder/adapters/WhereParams.adapter.ts
@@ -7,6 +7,7 @@ import {
 } from '../../filters'
 import { EnumType } from 'json-to-graphql-query'
 import { isInteger } from '../../../utils'
+import { CROSS_REFERENCE_PROPS } from '../../../constants'
 
 export class WhereParamsAdapter<TDocumentType> {
   adapt(params: TDocumentType | WhereLogicalOperator<TDocumentType>): any {
@@ -14,6 +15,8 @@ export class WhereParamsAdapter<TDocumentType> {
       return { ...params, operands: params.operands.map(this.adapt) }
     }
     const operations = Object.entries(params).map(([key, value]) => {
+      if (CROSS_REFERENCE_PROPS.includes(key))
+        return this.composeCrossReferenceParam({ key, value })
       if (value instanceof WhereCompareOperator)
         return this.composeParam({ key, ...value })
       return this.composeParam({
@@ -52,5 +55,23 @@ export class WhereParamsAdapter<TDocumentType> {
       path: param.key,
       ...this.adaptValue(param.value),
     }
+  }
+
+  private composeCrossReferenceParam(param: { key: any; value: any }) {
+    const operands = Object.entries(param.value).map(([prop, params]) => {
+      const adapted = this.adapt(params as any)
+      if (adapted instanceof WhereLogicalOperator)
+        adapted.operands.forEach(
+          (operation) => (operation.path = [param.key, prop, operation.path]),
+        )
+      else adapted.path = [param.key, prop, adapted.path]
+      return adapted
+    })
+    if (operands.length === 1) return operands[0]
+    else
+      return new WhereLogicalOperator({
+        operator: new EnumType(WhereOperator.And),
+        operands,
+      })
   }
 }
