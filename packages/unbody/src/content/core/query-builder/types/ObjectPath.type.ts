a9313cdf27f1c823f6700ecfecd50fb5ac1e3009
Hossein Mehrabi <ohyeaross@gmail.com>
2025-01-17 11:58:27 +0330

    fix: resolve missing date field keys in .select()

--- packages/unbody/src/content/core/query-builder/types/ObjectPath.type.ts
+++ packages/unbody/src/content/core/query-builder/types/ObjectPath.type.ts
@@ -2,6 +2,7 @@ import { HasArrayMember } from '../../../types'
 
 type Decrement<N extends number> = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10][N]
 type Ignore<K, Depth> = Depth extends 0 ? true : false
+type Includes<T, U> = Extract<T, U> extends never ? false : true
 
 type PathImplArray<
   T,
@@ -23,9 +24,11 @@ type PathImpl<T, K extends keyof T, Depth extends number> = K extends string
     : HasArrayMember<T[K]> extends true
     ? PathImplArray<T, K, Depth>
     : T[K] extends Record<string, any>
-    ? `${K}${Ignore<K, Decrement<Depth>> extends true
-        ? ''
-        : `.${PathImpl<T[K], keyof T[K], Decrement<Depth>>}`}`
+    ? Includes<T[K], Date> extends true
+      ? K
+      : `${K}${Ignore<K, Decrement<Depth>> extends true
+          ? ''
+          : `.${PathImpl<T[K], keyof T[K], Decrement<Depth>>}`}`
     : K
   : never
 
